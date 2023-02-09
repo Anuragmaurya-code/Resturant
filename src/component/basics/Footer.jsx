4bf37fed9f53de48c5648c11bc0e0545b4eae7d7
Anurag Maurya <mauryaanurag375@gmail.com>
2023-02-10 02:15:38 +0530

    adding

---
+++ src/component/basics/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer className='footer'>
+                <div className="conatiner grid grid-four-columm">
+                    <div className='f-about'>
+                    <h3>about</h3>
+                    <p>
+                        Ullamco nostrud ea fugiat ullamco fugiat voluptate est 
+                        labore exercitation ad sunt anim. Sit ad sit fugiat ex 
+                        et Lorem adipisicing et. Ullamco vol.
+                    </p>
+                    </div>
+                    <div className='f-links'>
+                        <h3>links</h3>
+                        <ul>
+                            <li><a href="#">home</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
--- src/component/basics/Resturant.jsx
+++ src/component/basics/Resturant.jsx
@@ -3,6 +3,7 @@ import "./styles.css"
 import Menu from "./menuApi"
 import MenuCard from "./MenuCard"
 import Navbar from "./Navbar"
+import Footer from './Footer'
 const uniqueList = [...new Set(Menu.map((curElem) => {//spread operator is necessory or else every element will be at 0 index of uniquelist
     return curElem.category;
 })),"All",]
@@ -26,6 +27,7 @@ const Restaurant = () => {
         <>
         <Navbar filterItem={filterItem} menuList={menuList}/>
         <MenuCard menuData={menuData} />
+        {/* <Footer/> */}
         </>
     )
 
